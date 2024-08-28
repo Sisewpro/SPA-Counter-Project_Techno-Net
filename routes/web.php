c29e14372598a57b02474d35ce75ed5a981ee658
Az Zikra Ismunandar <azzikra.ismunandar08@gmail.com>
2024-08-28 15:36:35 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CountTimeController; // Import controller untuk counttime
+use App\Http\Controllers\TimerCardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('auth.register');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +17,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/counttime', [CountTimeController::class, 'index'])->name('counttime');
 });
+
+Route::get('/dashboard', [TimerCardController::class, 'index'])->name('dashboard');
+Route::post('/timer-cards', [TimerCardController::class, 'store'])->name('timer-cards.store');
+Route::delete('/timer-cards/{id}', [TimerCardController::class, 'destroy'])->name('timer-cards.destroy');
+Route::post('/timer-cards/{id}/update', [TimerCardController::class, 'update'])->name('timer-cards.update');
+Route::put('/timer-cards/{id}', [TimerCardController::class, 'update'])->name('timer-cards.update');
 
 require __DIR__.'/auth.php';
